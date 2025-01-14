9bf56e3c1049fd08e5314452b2fb137e87dc8608
Tanvi Agrawal <tanviagrawal9913@gmail.com>
2025-01-14 16:40:06 +0530

    initial commit

--- Student.java
+++ Student.java
@@ -1,38 +1,48 @@
-class Student {
+class Student 
+{
     String name;
-    int rollNumber;
+    long enrollmentNumber;
     String course;
     int age;
 
-    public Student(String name, int rollNumber, String course, int age) {
+    public Student(String name,long enrollmentNumber,String course,int age)
+    {
         this.name = name;
-        this.rollNumber = rollNumber;
+        this.enrollmentNumber = enrollmentNumber;
         this.course = course;
         this.age = age;
     }
-
-    public void details() {
-        System.out.println("Name: " + name);
-        System.out.println("Roll Number: " + rollNumber);
-        System.out.println("Course: " + course);
-        System.out.println("Age: " + age);
-        System.out.println(); 
+    public void data()
+    {
+        System.out.println("Name: "+name);
+        System.out.println("Enrollment Number: "+enrollmentNumber);
+        System.out.println("Course name: "+course);
+        System.out.println("Age "+age);
     }
 }
-
-public class Main {
-    public static void main(String[] args) {
+public class Main
+{
+    public static void main(String[] args) 
+    {
         Student student1 = new Student("Vanshika Shrivastava", 35, "Computer Science And Engineering", 21);
         Student student2 = new Student("Ayushi Kothari", 5, "Mechanical Engineering", 22);
         Student student3 = new Student("Amit Sharma", 14, "Electrical And Electronics Engineering", 22);
 
         System.out.println("Student 1 Details:");
-        student1.details();
+        student1.data();
 
         System.out.println("Student 2 Details:");
-        student2.details();
+        student2.data();
 
         System.out.println("Student 3 Details:");
-        student3.details();
+        student3.data();
+
     }
+
+
+
+
+    
+
+
 }
